e091271168e3d375d1662d2c98c3d24469d2e4b3
paivalucasss <lucaspaiva2805@gmail.com>
2023-09-12 02:31:47 -0300

    Povoamento quase completo

---
+++ Povoamento.sql
@@ -0,0 +1,6 @@
+-- POVOAMENTO DO BANCO DE DADOS DO Detran
+
+INSERT INTO AutoEscola (nome, CNPJ) VALUES ('Auto Escola Petrolina', '12345678901234');
+INSERT INTO AutoEscola (nome, CNPJ) VALUES ('Auto Escola Socorro', '12345678901232');
+INSERT INTO AutoEscola (nome, CNPJ) VALUES ('Auto Escola Me Ajuda Deus', '12345678901554');
+
